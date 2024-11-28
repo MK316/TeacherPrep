dad1ead0fc57501723e9bad9a1f38e738b2dc00d
MK <99416359+MK316@users.noreply.github.com>
2024-11-28 15:12:07 +0900

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Application gallery for pre-service English teachers")
+st.write("In preparation")
+st.caption("Welcome to my app gallery, where I've curated a collection of tools designed and developed to aid in your preparation for the English teacher qualification exam. Each application has been crafted with the goal of supporting your personal study journey. I hope you find these resources beneficial as you work towards achieving your certification.")
